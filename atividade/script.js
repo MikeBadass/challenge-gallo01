723b5c7efc252f9eb9e9f49f5da073889c07ddd1
Henry Felipe <zoiohenry5@gmail.com>
2023-09-01 16:10:12 -0300

    modified: atividade

--- atividade/script.js
+++ atividade/script.js
@@ -1,15 +1,21 @@
+
 var numeroAleatorio = Math.floor(Math.random() * 100)
+var h2 = document.querySelector('h2')
+
+
+document.getElementById("btn-reiniciar").addEventListener("click", function () {
+    numeroAleatorio = Math.floor(Math.random() * 100)
+    h2.innerHTML = "";
+});
 
 function Comparar() {
     let numero = Number(document.getElementById('numero').value)
 
-    let h2 = document.querySelector('h2')
-
     if (numero == numeroAleatorio) {
-        h2.innerHTML = "Você acertou o número"
+        h2.innerHTML = "Você acertou o número!"
     } else if (numero < numeroAleatorio) {
-        h2.innerHTML = "Você chutou muito baixo"
+        h2.innerHTML = "Você chutou muito baixo!"
     } else {
-        h2.innerHTML = "Você chutou muito alto"
+        h2.innerHTML = "Você chutou muito alto!"
     }
 }
